192c56405e260097b0e227393e6ff344a0e39b78
Tim Chilcott <tim@afterwhile.com>
2025-06-19 12:17:38 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.sessions import SessionMiddleware
- main
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from .database import Base, engine, SessionLocal
