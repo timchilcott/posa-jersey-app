00a7cb78c6681ef1356bc5c8999cd1e7c6dac38c
Tim Chilcott <tim@afterwhile.com>
2025-09-29 15:40:50 -0700

    Add birth_year field to players model
    Added birth_year field to players model.

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class Player(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, nullable=False)
+    birth_year = Column(Integer, nullable=True)
     jersey_number = Column(Integer, nullable=True)
     parent_email = Column(String, nullable=False)
     locked = Column(Boolean, default=False)
